c2c212586fb8a347f0ff905d19e4d623a4c4066c
vasilie <vasilie.91@gmail.com>
2015-12-23 17:22:43 +0100

    carousel js, sass structure changes, added video, some class names changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,24 @@ $(document).ready(function(){
 		$(".nav").toggleClass("opened");
 		menu_icon.toggleClass("opened");
 	});
+	if (window.outterWidth < 1024) {
+	   $("#carousel__wrap").owlCarousel({
+		  	// navigation : true, // Show next and prev buttons
+		  	slideSpeed : 300,
+		  	paginationSpeed : 400,
+		  	singleItem:true
+	   });
+	   $("#gallery").owlCarousel({
+		  	// navigation : true, // Show next and prev buttons
+		  	slideSpeed : 300,
+		  	paginationSpeed : 400,
+		  	singleItem:true
+	   });
+	   $("#slider").owlCarousel({
+		  	// navigation : true, // Show next and prev buttons
+		  	slideSpeed : 300,
+		  	paginationSpeed : 400,
+		  	singleItem:true
+	   });
+	}
 });
